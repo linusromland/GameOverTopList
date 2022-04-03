f5d39d565019b735bfd1c747f47541b84d243715
Linus Romland <romlandlinus@gmail.com>
2022-04-03 13:41:14 +0200

    Consistent design for room

--- frontend/src/views/Rooms.vue
+++ frontend/src/views/Rooms.vue
@@ -31,12 +31,13 @@
 	<div class="flex flex-col items-center bg-slate-200 p-3 main-content">
 		<div class="flex items-center">
 			<h1 class="text-4xl font-semibold">Rummen</h1>
-			<CreateRoomModal @refresh="getRooms" />
 		</div>
+		<CreateRoomModal @refresh="getRooms" />
+
 		<div
 			v-for="(room, index) in rooms"
 			:key="index"
-			class="flex flex-col w-full md:w-2/4 lg:w-1/4 items-center bg-card m-3 p-2 rounded"
+			class="flex flex-col w-full md:w-2/4 lg:w-2/4 items-center bg-card m-3 p-2 rounded"
 		>
 			<h1 class="text-3xl text-black">{{ room.roomName }}</h1>
 			<div class="flex">
